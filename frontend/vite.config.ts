1e1dbd6bda120f414eb3134662e40621536e29cc
Rongrui Zhu <ronrzhu@gmail.com>
2025-11-11 22:30:32 -0800

    Frontend base migration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "DubPolls",
+  base: "SeattleNow",
   plugins: [react(), tailwindcss()],
 });
